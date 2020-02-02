3bfc235e56381fef7d6364863b7a556fcebfd9c4
haoyu <24222249@qq.com>
2020-02-02 16:17:03 +0800

    知识点3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,29 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:'Coder',
+    age:18,
+    students:[
+      {id:110,name:'john',age:20},
+      {id:111,name:'james',age:30},
+      {id:112,name:'curry',age:28},
+      {id:113,name:'jordan',age:18}
+    ],
+    counter: 0
   },
+    handleBtnClick(){
+      //1.错误做法：界面上是不会发生刷新的，只有在后台会改变
+      //this.data.counter += 1
+      //console.log(this.data.counter)
+      
+      //2.this.setData()
+      this.setData({
+        counter:this.data.counter +1
+      })
+    },
+    handleBtnClick2(){
+      this.setData({
+        counter:this.data.counter -1
+      })
+    }
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
