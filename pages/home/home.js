6b76e50dfc72c77e45c21db8eb741f307bc3ff59
haoyu <24222249@qq.com>
2020-02-06 20:46:22 +0800

    常见事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,75 @@ Page({
    * 页面的初始数据
    */
   data: {
+      titles:['衣服','裤子','鞋子']
+  },
+  handleBtnClick1(){
+    console.log('发生点击')
+ },
+  handleBtnClick2() {
+    console.log('发生点击')
+  },
+  handleTouchStart(){
+    console.log('handleTouchStart')
+  },
+  handleTouchMove(){
+    console.log('handleTouchMove')
+  },
+  handleTouchEnd() {
+    console.log('handleTouchEnd')
+  },
+  hanldeTap() {
+    console.log('hanldeTap')
+  },
+  handleLongpress(){
+    console('handleLongpress')
+  },
+  handleEventClick(event){
+    console.log('-----',event)
+  },
+  handleEventEnd(event){
+    console.log('+++++',event)
+  },
+  handleInner(event){
+    console.log(event)
+  },
+  handleOuter(event) {
+    console.log(event)
+  },
+  handleItemClick(event){
+    console.log(event)
+    //title - index
 
+    const dataset = event.currentTarget.dataset;
+    const title = dataset.item;
+    const index = dataset.index;
+    console.log(title,index) 
+  },
+  // --------事件冒泡和事件捕获------
+  handleCaptureView1(){
+    console.log('handleCaptureView1')
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBindView1(){
+    console.log('handleBindView1')
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
+  handleCaptureView2() {
+    console.log('handleCaptureView2')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleCaptureView3() {
+    console.log('handleCaptureView3')
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBindView3() {
+    console.log('handleBindView3')
   }
+
+
 })
